14bfd4975dca2814183822f050e1f839d524c466
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-28 22:20:24 +0200

    Updates

---
+++ precache-manifest.ba7098c5f847657fdecc5cbc7b6f3e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3b49587ffd3bd7b3f7837881e53bc0",
+    "url": "/SynergyWay/index.html"
+  },
+  {
+    "revision": "d93367d59c1fdb194e91",
+    "url": "/SynergyWay/static/css/main.dd082887.chunk.css"
+  },
+  {
+    "revision": "cd32179c30242167203b",
+    "url": "/SynergyWay/static/js/2.b7089791.chunk.js"
+  },
+  {
+    "revision": "d93367d59c1fdb194e91",
+    "url": "/SynergyWay/static/js/main.8a2f9e9f.chunk.js"
+  },
+  {
+    "revision": "9dc3d471c52223f772e7",
+    "url": "/SynergyWay/static/js/runtime-main.8fc165d9.js"
+  }
+]);
